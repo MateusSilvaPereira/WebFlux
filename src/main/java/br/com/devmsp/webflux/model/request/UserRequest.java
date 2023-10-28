24786fdf0cf5bb4bbcb1a46cf2416bb1a7b01f1d
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-10-28 11:56:50 -0400

    add exception handler validation error

--- src/main/java/br/com/devmsp/webflux/model/request/UserRequest.java
+++ src/main/java/br/com/devmsp/webflux/model/request/UserRequest.java
@@ -1,7 +1,21 @@
 package br.com.devmsp.webflux.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+
+
+        @NotBlank(message = "must not be null or empty")
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         String name,
+
+        @Email(message = "Invalid email")
+        @NotBlank(message = "must not be null or empty")
         String email,
+
+        @Size(min = 3, max = 20, message = "must be between 3 and 20 characters")
+        @NotBlank(message = "must not be null or empty")
         String password
 ) {}
